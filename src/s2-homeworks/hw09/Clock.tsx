c5fa093d40f733f95faa30c21720a46693bea7a3
Ilya Matsiushyn <matushin.97@gmail.com>
2023-04-08 19:02:02 +0300

    hw9 fixed

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -31,7 +31,7 @@ function Clock() {
     }
 
     const stringTime = date.toLocaleTimeString([], {hour12: false}) || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = date.toLocaleDateString() || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringDate = `${date.getDate().toString().padStart(2, '0')}.${(date.getMonth() + 1).toString().padStart(2, '0')}.${date.getFullYear()}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = (new Intl.DateTimeFormat('en-US', { weekday: 'long' })).format(date) || <br/> // пишут студенты
